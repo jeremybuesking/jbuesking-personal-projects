824279d7e94561dd365aec9467c1653932cfcffd
Jeremy Buesking <Jeremy_Buesking@insideranken.org>
2020-05-17 16:16:44 -0500

    Fresh MVC Project for example storefront.

---
+++ PublicStorefront/PublicStorefront/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PublicStorefront.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
